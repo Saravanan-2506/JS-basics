f728b02861a84617bc9a94f9dd93374b046ad690
Saravanan-2506 <rsaravanan2506@gmail.com>
2025-06-27 19:12:16 +0530

    File changes made

--- condition.js
+++ condition.js
@@ -1,6 +1,6 @@
-
+/*
 //1
-rain = true; 
+rain = false; 
 
 if (rain){
     console.log("Take an umbrella");
@@ -8,3 +8,23 @@ if (rain){
 else{
     console.log("Enjoy the sun");
 }
+*/
+
+function Triangle(a, b, c) {
+    if (a + b > c && a + c > b && b + c > a) {
+        if (a === b && b === c) {
+            return "Equilateral Triangle";
+        } else if (a === b || b === c || a === c) {
+            return "Isosceles Triangle";
+        } else {
+            return "Scalene Triangle";
+        }
+    } else {
+        return "Not a valid triangle";
+    }
+}
+
+console.log(Triangle(5, 5, 5)); 
+console.log(Triangle(5, 5, 3)); 
+console.log(Triangle(5, 6, 7));
+console.log(Triangle(1, 2, 3)); 
