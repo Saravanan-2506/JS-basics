9e1d2b9ebe985b5e3bb856b58ff8d17dff94a1b2
Saravanan-2506 <rsaravanan2506@gmail.com>
2025-06-30 19:25:10 +0530

    Grouped into folder

---
+++ Practice/condition.js
@@ -0,0 +1,31 @@
+/*
+//1
+rain = false; 
+
+if (rain){
+    console.log("Take an umbrella");
+}
+else{
+    console.log("Enjoy the sun");
+}
+
+
+function Triangle(a, b, c) {
+    if (a + b > c && a + c > b && b + c > a) {
+        if (a === b && b === c) {
+            return "Equilateral Triangle";
+        } else if (a === b || b === c || a === c) {
+            return "Isosceles Triangle";
+        } else {
+            return "Scalene Triangle";
+        }
+    } else {
+        return "Not a valid triangle";
+    }
+}
+
+console.log(Triangle(5, 5, 5)); 
+console.log(Triangle(5, 5, 3)); 
+console.log(Triangle(5, 6, 7));
+console.log(Triangle(1, 2, 3)); 
+*/
---
+++ Practice/practice.js
@@ -0,0 +1,15 @@
+/*
+var price = 1000;
+var product = "Book";
+var tax = 50;
+console.log(product);
+var total = price+tax;
+console.log (total);
+
+var fruitname = "Apple";
+var count = 10; 
+var fruitPrice = 15;
+var totalPrice = count * fruitPrice; 
+console.log(fruitname);
+console.log(totalPrice);
+*/
