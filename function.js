06a161cf4151b1dd487dfa4b4413280b781e2924
Saravanan-2506 <rsaravanan2506@gmail.com>
2025-06-30 16:36:17 +0530

    Changes made

--- function.js
+++ function.js
@@ -171,3 +171,24 @@ while (true) {
 }
 
 */
+
+//course practice
+const calcAverage = (a,b,c) => (a+b+c)/3 ;
+let scoreDolphins = calcAverage(44,23,71);
+let scoreKoalas = calcAverage(65,54,49);
+function checkWinner(avgDolphins,avgKoalas){
+    if (avgDolphins>=2*avgKoalas){
+        console.log("Dolphins win");
+    }
+    else if (avgKoalas>= 2*avgDolphins){
+        console.log("Koalas win");
+    }
+    else{
+        console.log("No team wins...");
+    }
+}
+checkWinner(scoreDolphins,scoreKoalas);
+
+scoreDolphins = calcAverage(85,54,41);
+scoreKoalas = calcAverage(23,34,27);
+checkWinner(scoreDolphins,scoreKoalas);
