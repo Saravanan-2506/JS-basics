cfb5bcfe3e15db20c904df1198ee00849b5326bf
Saravanan-2506 <rsaravanan2506@gmail.com>
2025-07-09 21:04:13 +0530

    Working changes made

--- Practice/practice.js
+++ Practice/practice.js
@@ -12,4 +12,27 @@ var fruitPrice = 15;
 var totalPrice = count * fruitPrice; 
 console.log(fruitname);
 console.log(totalPrice);
+
+//Write a program to swap two numbers using a temporary variable.
+let a ,b;
+a = prompt("Enter first number:");
+b = prompt("Enter second number:");
+console.log(`Values before swapping: ${a},${b}\n`);
+let temp = a;
+a=b;
+b=temp;
+console.log(`Values after swapping: ${a},${b}`);
 */
+//Use a while loop to reverse a number.
+function reverseNumber(num){
+    let reversed = 0;
+    while(num!=0){
+        let digit = num %10;
+        reversed = (reversed*10)+digit;
+        num=Math.floor(num/10);
+    }
+    return reversed;
+}
+let a = prompt("Enter a number:");
+console.log(`Entered number:${a}\n`);
+console.log(`Reversed number:${reverseNumber(a)}`);
