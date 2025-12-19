457e8504f60bb4792560950eb1cd465598d727bd
Saravanan-2506 <rsaravanan2506@gmail.com>
2025-12-19 18:14:15 +0530

    A better way

--- Functions/returnName.js
+++ Functions/returnName.js
@@ -2,16 +2,30 @@
 // returns "firstName lastName" if both are provided
 // returns only firstName if lastName is missing
 
-function formatName(firstName, lastName) {
+
+function formatName1(firstName, lastName) {
   if (!lastName) {
     return firstName;
   }
   return firstName + " " + lastName;
 }
 
-let name1 = formatName("R", "Saravanan");
-console.log(name1);
-let name2 = formatName("Nidharshanaa");
-console.log(name2);
-let name3 = formatName("Praveen", "Kumar");
-console.log(name3);
+let fullName = formatName1("R", "Saravanan");
+console.log(fullName);
+fullName = formatName1("Nidharshanaa");
+console.log(fullName);
+fullName = formatName1("Praveen", "Kumar");
+console.log(fullName);
+
+
+//A better way of approach
+function formatName2(firstName,lastName=''){
+    return lastName ? firstName+' '+lastName : firstName;
+}
+
+fullName = formatName2("R", "Saravanan");
+console.log(fullName);
+fullName = formatName2("Nidharshanaa");
+console.log(fullName);
+fullName = formatName2("Praveen", "Kumar");
+console.log(fullName);
